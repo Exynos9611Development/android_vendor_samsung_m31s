31b0a5f319d5904c805e3a088158ee7e705ab341
cat658011 <cat658011@gmail.com>
2026-01-27 13:33:25 +0500

    m31s-vendor: Regenerate vendor

--- m31s-vendor.mk
+++ m31s-vendor.mk
@@ -59,7 +59,6 @@ PRODUCT_PACKAGES += \
     libexynoscamera3 \
     libgf_in_system_lib \
     libqfp_sensortest \
-    libsecnativefeature \
     libsensorlistener \
     libsynaFpSensorTestNwd \
     libuniapi \
